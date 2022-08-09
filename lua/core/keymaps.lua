4d12d312608afbdafaf2920aa73eba35fb25f76e
me <me@hello.com>
2022-08-08 19:39:18 -0500

    open float lsp diagnostics with tab

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,9 +1,9 @@
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap= false, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = false, silent = true }
+	if opts then
+		options = vim.tbl_extend('force', options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 vim.g.mapleader = ' '
@@ -18,4 +18,5 @@ map('', '<right>', '<nop>')
 map('n', '<Esc><Esc>', ':nohl<CR>')
 map('n', 'K', ':m -2<cr>')
 map('n', 'J', ':m +1<cr>')
+map('n', '<Tab>', ':lua vim.diagnostic.open_float()<cr>')
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
